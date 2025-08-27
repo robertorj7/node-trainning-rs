a1839c0ad299e3b066521212b59bdc877e028fd0
robertorj7 <robertorj7@yahoo.com>
2025-08-27 17:46:39 -0300

    add transform stream example

--- 01 - Fundamentos-NodeJs/streams/fundamentals.js
+++ 01 - Fundamentos-NodeJs/streams/fundamentals.js
@@ -1,4 +1,4 @@
-import { Readable, Writable } from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
 class OneToHundredStream extends Readable {
   index = 1
@@ -24,4 +24,13 @@ class MultiplyByTenStream extends Writable {
   }
 }
 
-new OneToHundredStream().pipe(new MultiplyByTenStream())
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+    callback(null, Buffer.from(String(transformed)))
+  }
+} 
+
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream())
