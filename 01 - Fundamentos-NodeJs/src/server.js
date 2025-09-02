78fc62f9fe6d2183382b876d933e8f9511765a22
robertorj7 <robertorj7@yahoo.com>
2025-09-02 18:09:32 -0300

    add routes.js

--- 01 - Fundamentos-NodeJs/src/server.js
+++ 01 - Fundamentos-NodeJs/src/server.js
@@ -1,32 +1,18 @@
 import http from 'http'
-import { randomUUID } from 'node:crypto'
-import { json } from './middlewares/json.js';
 
-const database = new Database();
+import { json } from './middlewares/json.js';
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
     await json(req, res)
 
-    if (method === 'GET' && url === '/users') {
-        const users = database.select('users')
-        return res.end(JSON.stringify(users));
-    }
+    const route = routes.find(route => {
+        return route.method === method && route.path === url
+    })
 
-    if (method === 'POST' && url === '/users') {
-        const { name, email, age } = body
-
-        const user = {
-            id: randomUUID(),
-            name,
-            email,
-            age
-        }
-
-        database.insert('users', user)
-
-        return res.writeHead(201).end()
+    if (route) {
+        return route.handler(req, res)
     }
 
     return res.writeHead(404).end()
