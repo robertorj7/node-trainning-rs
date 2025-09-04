883501ba66ef212db888d692a7063d0c2f6ad83b
robertorj7 <robertorj7@yahoo.com>
2025-09-04 13:08:30 -0300

    regex correction

--- 01 - Fundamentos-NodeJs/src/utils/build-route-path.js
+++ 01 - Fundamentos-NodeJs/src/utils/build-route-path.js
@@ -1,4 +1,10 @@
 export function buildRoutePath(path) {
     const routeParametersRegex = /:([a-zA-Z]+)/g
-    console.log(Array.from(path.matchAll(routeParametersRegex)))
+    const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
+
+    const pathRegex = new RegExp(`^${pathWithParams.replace(/([()])/g, '\\$1')}$`)
+    return {
+        path: pathRegex,
+        params: Array.from(path.matchAll(routeParametersRegex)).map((match) => match[1])
+    }
 }
