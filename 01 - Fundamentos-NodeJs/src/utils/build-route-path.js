37d304f298db1c90d15d2f1e3e61650167db827b
robertorj7 <robertorj7@yahoo.com>
2025-09-06 21:07:34 -0300

    add query parameters

--- 01 - Fundamentos-NodeJs/src/utils/build-route-path.js
+++ 01 - Fundamentos-NodeJs/src/utils/build-route-path.js
@@ -2,9 +2,7 @@ export function buildRoutePath(path) {
     const routeParametersRegex = /:([a-zA-Z]+)/g
     const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
 
-    const pathRegex = new RegExp(`^${pathWithParams.replace(/([()])/g, '\\$1')}$`)
-    return {
-        path: pathRegex,
-        params: Array.from(path.matchAll(routeParametersRegex)).map((match) => match[1])
-    }
+    const pathRegex = new RegExp(`^${pathWithParams}($<query>\\?(.*))?$`)
+
+    return pathRegex
 }
