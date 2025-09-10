0245282b326db5a529ac8b421847550b674f56d1
robertorj7 <robertorj7@yahoo.com>
2025-09-10 18:36:00 -0300

    refactor server.js

--- 01 - Fundamentos-NodeJs/src/server.js
+++ 01 - Fundamentos-NodeJs/src/server.js
@@ -3,28 +3,33 @@ import http from 'http'
 import { json } from './middlewares/json.js';
 
 const server = http.createServer(async (req, res) => {
-    const { method, url } = req
+    try {
+        const { method, url } = req
 
-    await json(req, res)
+        await json(req, res)
 
-    const route = routes.find(route => {
-        return route.method === method && route.path.test(url)
-    })
+        const route = routes.find(route => route.method === method && route.path.test(url));
 
-    if (route) {
-        const routeParams = req.url.match(route.path)
+        if (route) {
+            const routeParams = req.url.match(route.path)
 
-        const { query, ...params } = routeParams.groups
+            const { query, ...params } = routeParams.groups
 
-        req.params = params
+            req.params = params
 
-        req.query = query ? extractQueryParams(query) : {}
+            req.query = query ? extractQueryParams(query) : {}
 
-        return route.handler(req, res)
+            return route.handler(req, res)
+        }
+
+        return res.writeHead(404).end()
+    } catch (error) {
+        console.error('Server error:', error);
+        res.writeHead(500).end('Internal Server Error');
     }
-
-    return res.writeHead(404).end()
 });
 
-server.listen(3333);
+server.listen(3333, () => {
+  console.log('Server is running on http://localhost:3333');
+});
 
