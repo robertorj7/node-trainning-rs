27cb6ccabefc30e53b4d081cdf640bef41de73a0
robertorj7 <robertorj7@yahoo.com>
2025-09-18 18:29:23 -0300

    knexfile configs

--- 02-api-rest-nodejs/src/database.ts
+++ 02-api-rest-nodejs/src/database.ts
@@ -1,8 +1,16 @@
+import type { Knex } from "knex"
 import knex from 'knex'
 
-export const db = knex({
-  client: 'sqlite',
+export const config: Knex.Config = {
+  client: 'sqlite3',
   connection: {
-    filename: './tmp/app.db'
+    filename: './db/app.db'
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations'
   }
-})
+}
+
+export const db = knex(config)
