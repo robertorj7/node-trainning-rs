628e0127a3283ebb8f486ca8d3f232d589f6df7f
robertorj7 <robertorj7@yahoo.com>
2025-09-26 16:05:51 -0300

    add fastify cookie

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,10 +1,12 @@
 import fastify from 'fastify';
+import cookie from '@fastify/cookie';
 
 import { env } from './env/index.ts';
 import { transactionsRoutes } from './routes/transactions.ts';
 
 const app = fastify()
 
+app.register(cookie)
 app.register(transactionsRoutes, { 
   prefix: 'transactions' 
 })
